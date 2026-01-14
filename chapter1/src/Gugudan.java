1b0e120c0879f2fc161285477b3bc87998168d7c
이충섭 <cndtjqdlqsle@gmail.com>
2026-01-14 16:40:11 +0900

    구구단

--- chapter1/src/Gugudan.java
+++ chapter1/src/Gugudan.java
@@ -1,9 +1,19 @@
 public class Gugudan {
     public static void main(String[] args) {
-        for (int i = 2; i <= 9; i++) {
+        for (int i = 2; i <= 9; i+=3) {
+            System.out.println( (i != 8) ?
+                    (i + "단 출력       " + (i + 1) + "단 출력       " + (i + 2) + "단 출력") :
+                    (i + "단 출력       " + (i + 1) + "단 출력") );
             for (int j = 1; j <= 9; j++) {
-                System.out.println(i + " * " + j + " = " + i * j);
+                String nums[] = new String[3];
+                for (int k = 0; k <= 2; k++)
+                    nums[k] = ((i+k)*j < 10) ? ((i+k) + " * " + j + " = " + (i+k) * j + "     ") : ((i+k) + " * " + j + " = " + (i+k) * j + "    ");
+
+                System.out.println( (i != 8) ?
+                        (nums[0] + nums[1] + nums[2]) :
+                        (nums[0] + nums[1]) );
             }
+            System.out.println("");
         }
     }
 }
