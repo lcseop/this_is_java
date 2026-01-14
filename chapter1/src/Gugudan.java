5b8688c0a3e429dc2e89db76c70885aab51e9e6b
이충섭 <cndtjqdlqsle@gmail.com>
2026-01-14 17:28:41 +0900

    구구단 수정

--- chapter1/src/Gugudan.java
+++ chapter1/src/Gugudan.java
@@ -1,13 +1,16 @@
 public class Gugudan {
     public static void main(String[] args) {
-        for (int i = 2; i <= 9; i+=3) {
+        final String SHORT_SPACE = "    ";
+        final String LONG_SPACE = "     ";
+
+        for (int i = 2; i <= 9; i += 3) {
             System.out.println( (i != 8) ?
-                    (i + "단 출력       " + (i + 1) + "단 출력       " + (i + 2) + "단 출력") :
-                    (i + "단 출력       " + (i + 1) + "단 출력") );
+                    (i + "단 출력  " + LONG_SPACE + (i + 1) + "단 출력  " + LONG_SPACE + (i + 2) + "단 출력") :
+                    (i + "단 출력  " + LONG_SPACE + (i + 1) + "단 출력") );
             for (int j = 1; j <= 9; j++) {
                 String nums[] = new String[3];
                 for (int k = 0; k <= 2; k++)
-                    nums[k] = ((i+k)*j < 10) ? ((i+k) + " * " + j + " = " + (i+k) * j + "     ") : ((i+k) + " * " + j + " = " + (i+k) * j + "    ");
+                    nums[k] = ((i+k)*j < 10) ? ((i+k) + " * " + j + " = " + (i+k) * j + LONG_SPACE) : ((i+k) + " * " + j + " = " + (i+k) * j + SHORT_SPACE);
 
                 System.out.println( (i != 8) ?
                         (nums[0] + nums[1] + nums[2]) :
