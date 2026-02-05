2a351cdf0d9083937af1ad6e26ead73aa0f5d8d5
이충섭 <cndtjqdlqsle@gmail.com>
2026-02-05 15:26:54 +0900

    Chapter 11 예외처리 개인 과제 strongsum 추가

--- chapter11/src/main/java/com/mjc813/report/report01/Mjc813Calculator.java
+++ chapter11/src/main/java/com/mjc813/report/report01/Mjc813Calculator.java
@@ -3,11 +3,19 @@ package com.mjc813.report.report01;
 import java.nio.channels.NotYetBoundException;
 
 public class Mjc813Calculator {
+	public Long strongSum(Integer ... arrs) {
+		Long result = 0L;
+
+		for (Integer num : arrs) {
+			if (num != null) {
+				result += num;
+			}
+		}
+
+		return result;
+	}
 	public Long sum( Integer ... arrs ) throws NullPointerException {
 		Long result = 0L;
-		if (arrs == null) {
-			throw new NullPointerException();
-		}
 		// arrs 값들의 모든 합을 구해야 합니다.
 		// 다만 arrs 배열이 null 일 수 있으므로 예외처리를 이곳이든 이곳을 호출하는 메소드에서든 꼭 해야 합니다.
 		for (int num : arrs) {
@@ -17,9 +25,6 @@ public class Mjc813Calculator {
 	}
 
 	public Long sum( Integer[] arrs, Integer start, Integer end) throws NullPointerException, ArrayIndexOutOfBoundsException {
-		if (arrs == null || start == null || end == null) {
-			throw new NullPointerException();
-		}
 		Long result = 0L;
 		// arrs 배열중에서 start ~ end 인덱스 까지의 원소들의 모든 합을 구해야 합니다.
 		// 다만 arrs 배열이 null 일 수 도 있고, 원소가 null 일 수도 있고
