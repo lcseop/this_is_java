26fc7e6fc93719dd5c874a5734d303a175238ff7
이충섭 <cndtjqdlqsle@gmail.com>
2026-06-16 21:30:56 +0900

    Member findAll 관리자만 볼 수 있게 변경

--- session_login/src/main/java/com/mjc813/session_login/model/member/MemberRestController.java
+++ session_login/src/main/java/com/mjc813/session_login/model/member/MemberRestController.java
@@ -4,6 +4,7 @@ import com.mjc813.session_login.common.ComResponseDto;
 import com.mjc813.session_login.common.ResponseCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,7 +24,13 @@ public class MemberRestController {
 	}
 
 	@GetMapping("")
-	public ResponseEntity<ComResponseDto<List<MemberDto>>> findAll() {
+	public ResponseEntity<ComResponseDto<List<MemberDto>>> findAll(Model model) {
+		Object obj = model.getAttribute("signedMember");
+		if (obj instanceof IMember signedMember && !Role.ADMIN.toString().equals(signedMember.getRole())) {
+			return ResponseEntity.status(403).body(
+					ComResponseDto.make(ResponseCode.AUTHORIZATION_ERROR, null)
+			);
+		}
 		List<MemberDto> result = this.memberService.findAll();
 		return ResponseEntity.status(200).body(
 				ComResponseDto.make(ResponseCode.SUCCESS, result)
